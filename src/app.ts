11e82f45117dcca0bd6d45b65dc09a8391676ab9
mahmudulhaque13 <m.h.upal13@gmail.com>
2026-06-10 07:26:00 +0600

    app.ts added

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,25 @@ import express, {
   type Request,
   type Response,
 } from "express";
+import { authRouter } from "./modules/auth/auth.route";
+import { issueRouter } from "./modules/issues/issue.route";
+import { userRouter } from "./modules/users/user.route";
+import globalErrorHandler from "./middleware/globalErrorHandler";
 
 const app: Application = express();
 
+app.use(express.json());
+
 app.get("/", (req: Request, res: Response) => {
-  res.send("hello world");
+  res
+    .status(200)
+    .json({ success: true, message: "DevPulse API Engine Operational." });
 });
+
+app.use("/api/auth", authRouter);
+app.use("/api/issues", issueRouter);
+app.use("/api/users", userRouter);
+
+app.use(globalErrorHandler);
 
 export default app;
